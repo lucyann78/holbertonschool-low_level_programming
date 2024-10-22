071866e03610280b1e561c848737f4c4b62225ed
lucyann <lucyann78@gmail.com>
2024-10-22 09:43:04 -0400

    Printf Finally

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -6,8 +6,9 @@
  *
  */
 int main(void)
-{printf("with proper grammar, but the outcome is a piece of art,\n);
-return (0);}
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
 
 
-gcc -Wall -o program.c
