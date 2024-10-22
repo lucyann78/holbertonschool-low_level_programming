3b350dfc46f9fbbbe6046601a58d7d58e8176b52
lucyann <lucyann78@gmail.com>
2024-10-22 09:49:07 -0400

    Size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,9 +6,11 @@
  * Return: Always 0
  */
 int main(void)
-{printf("Size of char: %zu
+
+{
+	printf("Size of char: %zu
 byte(s)\n", sizeof(char));
-printf(Size of int : % zu byte(s)\n,
+printf("Size of int : % zu byte(s)\n",
 sizeof(int));
 printf("Size of long: %zu byte(s)\n",
 sizeof(long));
@@ -16,4 +18,5 @@ printf("Size of long long: %zu
 byte(s)\n", sizeof(Long long));
 printf("Size of float: %zu
 byte(s)\n", sizeof(float));
-return (0); }
+return (0);
+}
