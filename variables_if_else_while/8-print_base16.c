08272e3f81b33606a297d0837567741289b08ff0
lucyann <lucyann78@gmail.com>
2024-10-24 11:07:30 -0400

    Numbers base sixteen

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int i = 0;
+
+	while (i < 16)
+	{
+		if (i < 10)
+		{
+			putchar(i + '0');
+		}
+	else
+	{
+		putchar(i - 10 + 'a');
+	}
+	i++;
+}
+	putchar('\n');
+	return (0);
+}
+
