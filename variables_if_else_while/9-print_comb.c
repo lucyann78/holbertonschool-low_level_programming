24cf3fc66162868088d69d72ed8600af50d440ed
lucyann <lucyann78@gmail.com>
2024-10-24 13:05:47 -0400

    Print combinations

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+int i = 0;
+while (i < 10)
+{
+	putchar(i + O);
+	if (i < 9)
+{
+	putchar(',');
+	putchar(' ');
+}
+	i++;
+}
+putchar('\n');
+return (0);
+}
