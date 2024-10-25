228a3fd8e3eea6a358fea03b8ebae0ae4708f1be
lucyann <lucyann78@gmail.com>
2024-10-25 11:35:00 -0400

    Alphabetx10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -11,12 +11,12 @@ void print_alphabet_x10(void)
 	char c;
 	int i = 0;
 
-	while (i <= 9)
+	while (i <= 10)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{	_putchar(c);
 		}
+		i++;
+		_putchar('\n');
 	}
-	_putchar('\n');
-	i++;
 }
