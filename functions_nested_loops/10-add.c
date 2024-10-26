3ce5b65630f4f332d30e09f91f9bc11b0f678150
lucyann <lucyann78@gmail.com>
2024-10-26 19:08:11 -0400

    10-add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @n1: The first integer to be added
+ * @n2: The second integer to be added
+ * Return: Integer value
+ */
+
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+}
