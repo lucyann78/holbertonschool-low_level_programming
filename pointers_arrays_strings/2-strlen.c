c6da1af1e1647d43eda59d19c477be570b44be5b
lucyann <lucyann78@gmail.com>
2024-10-29 16:13:27 -0400

    Strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _strlen - Returns the length of a string
+ * @str: The string whose length is to be calculated
+ * Return: The total count of character or length of string
+ */
+
+int _strlen(const char *str)
+
+{
+	int length = 0;
+
+	while (str[length] != '\n')
+	{
+	length++;
+	}
+	return (length);
+}
