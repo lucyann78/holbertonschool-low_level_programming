6b44495f1d23c9a615abe4adfd4f15bcf05432fd
lucyann <lucyann78@gmail.com>
2024-10-29 04:01:02 -0400

    Print a square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - Print a square of size "size" using # char
+ * @ size: the size of the square to be printed
+ * Return: Square figure
+ */
+
+void print_square(int size)
+
+{
+	int x, y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
