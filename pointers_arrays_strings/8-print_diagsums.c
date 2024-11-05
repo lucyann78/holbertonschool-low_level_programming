57d66e2831b9aef7f14213715bba1819ebe9aea7
lucyann <lucyann78@gmail.com>
2024-11-05 17:43:21 -0400

    diagsums corrected

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -17,7 +17,6 @@ void print_diagsums(int *a, int size)
 	sum1 += *(a + (i * size + i));
 	sum2 += *(a + (i * size + (size - 1 - i)));
 	}
-		printf("%d\n", sum1);
-		printf("%d\n", sum2);
+		printf("%d, %d\n", sum1, sum2);
 }
 
