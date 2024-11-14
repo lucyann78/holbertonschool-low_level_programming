d8ef54a836b3d612e40c99e7c901e1cace8aeeaf
lucyann <lucyann78@gmail.com>
2024-11-14 15:49:05 -0400

    string nconcat

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * string_nconcat - Concatenates two strings
+ * @s1: The first string
+ * @s2: The second string
+ * @n: maximun number of bytes to concatenate from s2
+ * Return: a pointer to the newly allocated space memory
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i = 0, j = 0, k = 0, l = 0;
+	char *str;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[i])
+		i++;
+	while (s2[k])
+		k++;
+	if (n >= k)
+	{
+		l = i + k;
+	}
+	else
+	{
+		l = i + n;
+	}
+	str = malloc(sizeof(char) * (l + 1));
+	if (str == NULL)
+		return (NULL);
+	while (j < l)
+	{
+		if (j < i)
+		{
+			str[j] = s1[j];
+		}
+		else
+		{
+			str[j] = s2[k];
+			k++;
+		}
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
+}
+
