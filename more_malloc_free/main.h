56444596e26c36ba6c6a33f2bd0cce02ed0f1950
lucyann <lucyann78@gmail.com>
2024-11-14 13:59:44 -0400

    main.h corrected

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 void *malloc_checked(unsigned int b);
 
 char *string_nconcat(char *s1, char *s2, unsigned int n);
@@ -6,4 +9,6 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 
 int *array_range(int min, int max);
 
-int _putchar.c
+int _putchar(char c);
+
+#endif
